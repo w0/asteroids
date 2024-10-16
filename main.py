f769f7ef5c997097c44d5f622ac55dc418cb3b06
w0 <33212583+w0@users.noreply.github.com>
2024-10-15 21:43:55 -0500

    flying rocks

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(surface, color="black")
+        for object in updatable:
+            object.update(dt)
+
+        screen.fill("black")
+
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = game_clock.tick(60) / 1000
 
 
     print("Starting asteroids!")
